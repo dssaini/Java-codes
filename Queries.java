e1ef5147ad240b97c25b9d05b0990ad12d6438d4
Admin <admin@hawksindia.in>
2023-11-10 20:21:00 +0530

    Jdbc Tutorila Added

---
+++ Queries.java
@@ -0,0 +1,7 @@
+public class Queries {
+    static String CreateDb = "create database Test02";
+    static String CreateTable= "create table student (Student_id int,Student_name varchar,Student_class varchar) ";
+    static String insertQuery = "insert into student(Student_id, Student_name, Student_class)values(1,'Deepak Saini','class_8')";
+    static String updateQuesry = "update student where Student_id=1";
+
+}
